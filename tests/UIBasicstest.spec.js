e7ab26a3bdb2e440863e189329b9cb4bb735b470
manuel76046 <kenseith92@hotmail.com>
2025-08-13 10:28:12 -0500

    First commit

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,13 @@
+const {test} = require('@playwright/test');
+
+
+test('First  Playwright test',async ()=>
+{
+//Playwright code
+//Step 1 - Open Browser
+
+//Step 2 - Enter username/password 2 seconds
+
+//Step 3 - Click login button
+
+});
